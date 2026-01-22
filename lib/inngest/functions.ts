1856b3e6826ffab6addf10ca38f55072555db2fb
Ulsbold12 <ulsboldulsbold921@gmail.com>
2026-01-22 16:53:08 +0800

    auth logic

---
+++ lib/inngest/functions.ts
@@ -0,0 +1,59 @@
+import { sendWelcomeEmail } from "../nodemailer";
+import { inngest } from "./client";
+import { PERSONALIZED_WELCOME_EMAIL_PROMPT } from "./prompts";
+
+export const sendSignUpEmail = inngest.createFunction(
+  { id: "sign-up-email" },
+  { event: "app/user.created" },
+  async ({ event, step }) => {
+    const userProfile = `
+- Country: ${event.data.country}
+- Investment goals: ${event.data.investmentGoals}
+- Risk tolerance: ${event.data.riskTolerance}
+- Preferred industry: ${event.data.preferredIndustry}
+`.trim();
+
+    const prompt = PERSONALIZED_WELCOME_EMAIL_PROMPT.replace(
+      "{{userProfile}}",
+      userProfile,
+    );
+
+    const response = await step.ai.infer("generate-welcome-intro", {
+      model: step.ai.models.gemini({ model: "gemini-3-flash-preview" }),
+      body: {
+        contents: [
+          {
+            role: "user",
+            parts: [{ text: prompt }],
+          },
+        ],
+      },
+    });
+
+    await step.run("send-welcome-email", async () => {
+      const part = response?.candidates?.[0]?.content?.parts?.[0];
+
+      const introText =
+        (part &&
+        typeof part === "object" &&
+        "text" in part &&
+        typeof (part as any).text === "string"
+          ? (part as any).text
+          : null) ??
+        "Thanks for joining Signalist. You now have the tools to track markets and make smarter moves.";
+      const {
+        data: { email, name },
+      } = event;
+      return await sendWelcomeEmail({
+        email,
+        name,
+        intro: introText,
+      });
+    });
+
+    return {
+      success: true,
+      message: "Welcome email sent successfully",
+    };
+  },
+);
