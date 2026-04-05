0243f70350a6873a7ae1ac69befdb918ddb75d7e
Ulsbold12 <ulsboldulsbold921@gmail.com>
2026-04-05 21:43:11 +0800

    ad

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,31 @@
+// "use serve";
+
+// import { connectToDatabase } from "@/database/mongoose";
+// import { M_PLUS_1 } from "next/font/google";
+
+// export const getAllUsersForNewsEmail = async () => {
+//   try {
+//     const mongoose = await connectToDatabase;
+//     const db = mongoose.connection.db;
+//     if (!db) throw new Error("Mongoose connection not connected");
+
+//     const users = await db
+//       .collection("user")
+//       .find(
+//         { email: { $exists: true, $ne: null } },
+//         { projection: { _id: 1, id: 1, email: 1, name: 1, country: 1 } },
+//       )
+//       .toArray();
+
+//     return users
+//       .filter((user) => user.email && user.name)
+//       .map((user) => ({
+//         id: user.id || user._id?.toString() || "",
+//         email: user.email,
+//         name: user.name,
+//       }));
+//   } catch (e) {
+//     console.error(" error fetching users for news email:", e);
+//     return [];
+//   }
+// };
--- middleware/index.ts
+++ middleware/index.ts
@@ -15,4 +15,5 @@ export const config = {
   matcher: [
     "/((?!api|_next/static|_next/image|favicon.ico|sign-in|sign-up|assets).*)",
   ],
-};
+}
+
